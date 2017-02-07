f47443b91cb4c1e399d9259281749d0bfc85a5a4
John Rummell <john@ctrlaltdevelopment.com>
2017-02-06 21:23:18 -0500

    Fixed $.get() fail handler, now removing the item from the dom after calling the itemRemoved handler, updated jquery to 2.2.4

--- jquery.dynamiclist/gulpfile.js
+++ jquery.dynamiclist/gulpfile.js
@@ -1,22 +1,27 @@
 ﻿/// <binding AfterBuild='uglify' />
 var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
+var eol = require('gulp-eol');
 
 gulp.task('uglify', function ()
 {
     gulp.src('scripts/package/jquery.dynamiclist.js')
+        .pipe(eol())
         .pipe(uglify('jquery.dynamiclist.min.js', { outSourceMap: true }))
         .pipe(gulp.dest('scripts/package'));
 
     gulp.src('scripts/package/jquery.dynamiclist.templates.bootstrap.js')
+        .pipe(eol())
         .pipe(uglify('jquery.dynamiclist.templates.bootstrap.min.js', { outSourceMap: true }))
         .pipe(gulp.dest('scripts/package'));
 
     gulp.src('scripts/package/jquery.dynamiclist.templates.kendo.js')
+        .pipe(eol())
         .pipe(uglify('jquery.dynamiclist.templates.kendo.min.js', { outSourceMap: true }))
         .pipe(gulp.dest('scripts/package'));
 
     gulp.src('scripts/package/jquery.validate.unobtrusive.dynamic.js')
+        .pipe(eol())
         .pipe(uglify('jquery.validate.unobtrusive.dynamic.min.js', {outSourceMap: true}))
         .pipe(gulp.dest('scripts/package'));
 });
