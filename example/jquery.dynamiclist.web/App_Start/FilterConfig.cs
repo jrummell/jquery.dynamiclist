700d7e010958e89dc7d2ef12a97e39ac9b25dfb4
John Rummell <john@ctrlaltdevelopment.com>
2026-08-22 13:41:24 -0400

    Rename project to jquery.dynamiclist.web and upgrade tooling
    Renamed solution, project, and namespaces to jquery.dynamiclist.web. Updated Visual Studio and pipeline versions, upgraded .csproj to ToolsVersion 15.0, and refreshed NuGet packages. Added favicon.ico and moved validation script to node_modules. Updated Web.config for MVC 5.2.9, added Roslyn support, and cleaned up config files and comments. Explicitly included key views in project file.

--- example/jquery.dynamiclist.web/App_Start/FilterConfig.cs
+++ example/jquery.dynamiclist.web/App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace jquery.dynamiclist
+namespace jquery.dynamiclist.web
 {
     public class FilterConfig
     {
--- example/jquery.dynamiclist.web/Properties/AssemblyInfo.cs
+++ example/jquery.dynamiclist.web/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("jquery.dynamiclist")]
+[assembly: AssemblyTitle("jquery.dynamiclist.web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("jquery.dynamiclist")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("jquery.dynamiclist.web")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ffa6a787-5ffa-48bb-aeec-b8989c76427f")]
+[assembly: Guid("8f8143a2-ed64-491e-a62d-ed1d338a1fc9")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
